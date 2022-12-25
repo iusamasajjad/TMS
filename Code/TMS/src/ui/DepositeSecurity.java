a60322ed3e1af12c0fbf6b03a3107ac19de69408
Usama Sajjad <i.usamasajjad@gmail.com>
2022-12-25 16:10:24 +0500

    screens connected
    screens connected with each other

--- Code/TMS/src/ui/DepositeSecurity.java
+++ Code/TMS/src/ui/DepositeSecurity.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ui;
-
+import ui.users.SignIn;
 /**
  *
  * @author HAMZA
--- Code/TMS/src/ui/users/Register.java
+++ Code/TMS/src/ui/users/Register.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ui.users;
-
+import ui.DepositeSecurity;
 /**
  *
  * @author HAMZA
