51082a7d8452d10db28047edee24a7e5a0c30e2a
Usama Sajjad <i.usamasajjad@gmail.com>
2022-12-25 17:26:10 +0500

    packages added in project
    Database connection and other packages added in project

---
+++ Code/TMS/src/Model/userManagement/SMSFactory.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.userManagement;
+
+import DataAccess.DataControl.DALManager;
+import DataAccess.DataControl.RecordsAdder;
+import DataAccess.DataControl.RecordsMapper;
+import DataAccess.DataControl.RecordsModifier;
+import Model.dto.Response;
+
+
+public class SMSFactory {
+
+    static DALManager getInstanceOfDALManager() {
+        return new DALManager(new RecordsMapper());
+    }
+
+    public static OSSController getInstanceOfSMSController() {
+        return new OSSController();
+    }
+
+    static Response getResponseInstance() {
+        return new Response();
+    }
+
+    public static RecordsAdder getInstanceOfAdder() {
+        return new RecordsAdder();
+    }
+
+    public static RecordsModifier getInstanceOfModifier() {
+        return new RecordsModifier();
+    }
+    
+}
---
+++ Code/TMS/src/Model/validator/CommonValidator.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.validator;
+
+import Model.dto.EmployeeDTO;
+import Model.dto.Message;
+import Model.dto.MessageType;
+import Model.dto.Response;
+
+
+public class CommonValidator {
+
+    public static void validateEmployee(EmployeeDTO objEmp, Response objResponse) {
+        if(objEmp.FirstName == null || objEmp.FirstName.length() < 3){
+            objResponse.messagesList.add(new Message("FIrst Name is not valid, Provide valid first name with at least 3 characters.",MessageType.Error));
+        }else if(objEmp.Address.length() <10){
+            objResponse.messagesList.add(new Message("Address is not valid, Provide valid Address with at least 10 characters.",MessageType.Error));
+        }else if(objEmp.Country.length() <4){
+            objResponse.messagesList.add(new Message("Country is not valid, Provide valid Country with at least 3 characters.",MessageType.Error));
+        }else if(objEmp.BirthDate == null){
+            objResponse.messagesList.add(new Message("Birth Date is not valid, Provide valid Birth Date.",MessageType.Error));
+        }else if(objEmp.Title == null){
+            objResponse.messagesList.add(new Message("Title is not valid, Provide valid Title.",MessageType.Error));
+        }else if(objEmp.City == null){
+            objResponse.messagesList.add(new Message("City should not null, Provide valid City.",MessageType.Error));
+        }
+        // Todo validate the rest
+    }
+    
+}
